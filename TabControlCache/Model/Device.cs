20753021dcc48d84ea26bb839f65792ee2b7d6b0
vk1408 <84286885+vk1408@users.noreply.github.com>
2024-02-20 22:08:54 +0100

    Add project files.

---
+++ TabControlCache/Model/Device.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabControlCache.Model
+{
+    public class Device
+    {
+        public Device(int number, string name, string description)
+        {
+            Number = number;
+            Name = name;
+            Description = description;
+        }
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
---
+++ TabControlCache/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using TabControlCache.Model;
+using TabControlCache.MVVM;
+
+namespace TabControlCache.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            for (int i = 0; i < 100; i++) 
+            {
+                Devices.Add(new Device(i, $"Device {i}", $"Device description {i}"));
+            }
+        }
+        // Tab item cache to boost perfomance. No content rendering at tab switch! 
+        private readonly Dictionary<string, UIElement> _tabContentCache = new Dictionary<string, UIElement>();
+        private object _tabItem;
+        public object TabItem 
+        {  
+            get => _tabItem; 
+            set
+            {
+                if (_tabItem != value) 
+                { 
+                    _tabItem = value;
+                    OnPropertyChanged();
+                    var selectedTab = value as TabItem;
+                    string header = selectedTab.Header.ToString();
+                    if (!_tabContentCache.ContainsKey(header))
+                    {
+                        // Load content for the selected TabItem
+                        var content = selectedTab.Content;
+                        _tabContentCache[header] = (UIElement) content;
+                        selectedTab.Content = content;
+                    }
+                    else
+                    {
+                        selectedTab.Content = _tabContentCache[header];
+                    }
+                }
+            }
+        }
+
+        private List<Device> _devices = new List<Device>();
+
+        public List<Device> Devices
+        {
+            get => _devices;
+            set 
+            { 
+                if (_devices != value) 
+                { 
+                    _devices = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+    }
+}
